0bca69f3eee978c9c42ee827957a6e2b41dd4c1f
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-09-19 15:29:43 +0300

    added profile display

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,43 @@
 import { useContext } from "react"
 import { ThemeContext } from "../theme-provider"
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react"
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react"
 import { FaRegMoon } from "react-icons/fa"
 import { LuSunMedium } from "react-icons/lu"
+import { logout, selectIsAuthenticated } from "../../features/user/userSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
-export const Header = () =>{
-  const {theme, toggleTheme} = useContext(ThemeContext)
+import { CiLogout } from "react-icons/ci"
 
-  return(
+
+export const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
+
+  return (
     <Navbar>
       <NavbarBrand>
         <p className="font-bold text-inherit">Network Social</p>
       </NavbarBrand>
-      <NavbarContent justify='end'>
+      <NavbarContent justify="end">
         <NavbarItem
-          className='lg:flex text-3x1 cursor-pointer'
+          className="lg:flex text-3x1 cursor-pointer"
           onClick={() => toggleTheme()}
         >
-          {theme === 'light' ? <FaRegMoon /> : <LuSunMedium />}
+          {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
         </NavbarItem>
         <NavbarItem>
-
+          {isAuthenticated && (
+            <Button color="default" variant="flat" className="gap-2" onClick={handleLogout}>
+              <CiLogout /><span>Выйти</span></Button>)}
         </NavbarItem>
       </NavbarContent>
     </Navbar>
